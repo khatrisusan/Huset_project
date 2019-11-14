4b948a54e0b4407d93cbeb33c530a4697e040eed
khatrisusan <khatrisusan>
2019-11-14 06:02:11 +0100

    added category.html, and removed unused category

--- main.js
+++ main.js
@@ -28,55 +28,59 @@ function showPost(post) {
 */
 
 
-function show(){
+function show() {
     document.querySelector("#nav").classList.toggle("hide")
 }
 
 
-function getNav(){
+function getNav() {
     console.log('get nav running')
     fetch("http://cosmicstryder.dk/wordpress/wp-json/wp/v2/categories")
-		.then(res => res.json())
-		.then(getCategory)
+        .then(res => res.json())
+        .then(getCategory)
 }
-function getCategory(categoriesJson) {
-	categoriesJson.forEach(function(e){
-        const newElement = document.createElement("a");
-        newElement.textContent=e.name;
-        newElement.setAttribute("href", "sub.html?id=" + e.id)
-        document.querySelector("#nav").appendChild(newElement);
-        console.log(newElement);
 
+function getCategory(oneItem) {
+    oneItem.forEach(function (e) {
+        console.log(e);
+        if (e.parent === 0 && e.count > 0) {
+            const newElement = document.createElement("a");
+            newElement.textContent = e.name;
+            newElement.setAttribute("href", "index.html")
+            document.querySelector("#nav").appendChild(newElement);
+            //console.log(newElement);
+
+        }
     })
 }
 getNav();
 window.addEventListener("DOMContentLoaded", getData);
 
 function getData() {
-	const urlParams = new URLSearchParams(window.location.search);
-	const search = urlParams.get("search");
-	const id = urlParams.get("id");
+    const urlParams = new URLSearchParams(window.location.search);
+    const search = urlParams.get("search");
+    const id = urlParams.get("id");
     const category = urlParams.get("category");
 
-	if (search) {
-		console.log("this is searching")
-		getSearchData();
-	} else if (id) {
-		console.log("hi mom")
-		//getSingleBook();
-	} else {
-		//console.log("Not searching")
-		getFrontpageData();
-	}
+    if (search) {
+        console.log("this is searching")
+        getSearchData();
+    } else if (id) {
+        console.log("hi mom")
+        //getSingleBook();
+    } else {
+        //console.log("Not searching")
+        getFrontpageData();
+    }
 
 }
 
 function getSearchData() {
-	const urlParams = new URLSearchParams(window.location.search);
-	const search = urlParams.get("search");
-	fetch("http://cosmicstryder.dk/wordpress/wp-json/wp/v2/event?_embed&search=" + search)
-		.then(res => res.json())
-		.then(handleData)
+    const urlParams = new URLSearchParams(window.location.search);
+    const search = urlParams.get("search");
+    fetch("http://cosmicstryder.dk/wordpress/wp-json/wp/v2/event?_embed&search=" + search)
+        .then(res => res.json())
+        .then(handleData)
 }
 /*
 ==========================================
@@ -89,35 +93,36 @@ fetch category
 
 
 function getFrontpageData() {
-	fetch("http://cosmicstryder.dk/wordpress/wp-json/wp/v2/event?_embed")
-		.then(res => res.json())
-		.then(handleData)
+    fetch("http://cosmicstryder.dk/wordpress/wp-json/wp/v2/event?_embed")
+        .then(res => res.json())
+        .then(handleData)
 }
-    function handleData(myData) {
-	myData.forEach(showPost);
+
+function handleData(myData) {
+    myData.forEach(showPost);
 }
 
 function showPost(post) {
-	//console.log(post);
+    //console.log(post);
 
-	const template = document.querySelector(".postTemplate").content;
-	const postCopy = template.cloneNode(true);
+    const template = document.querySelector(".postTemplate").content;
+    const postCopy = template.cloneNode(true);
 
-	postCopy.querySelector("h1").innerHTML = post.title.rendered;
+    postCopy.querySelector("h1").innerHTML = post.title.rendered;
     postCopy.querySelector("img").src = post.event_image.guid;
     postCopy.querySelector("section.description p").innerHTML = post.content.rendered;
 
 
-	/*
+    /*
 change DOM CONTENT
 */
-	//const img = postCopy.querySelector("img");
-	//img.setAttribute("src", imgPath);
+    //const img = postCopy.querySelector("img");
+    //img.setAttribute("src", imgPath);
 
-	//img.setAttribute("alt", "cover of the book"+ post.title.rendered);
+    //img.setAttribute("alt", "cover of the book"+ post.title.rendered);
 
-	const a = postCopy.querySelector("a");
-	//a.href = "subpage.html?id=" + post.id
-	document.querySelector("#posts").appendChild(postCopy);
+    const a = postCopy.querySelector("a");
+    //a.href = "subpage.html?id=" + post.id
+    document.querySelector("#posts").appendChild(postCopy);
 
 }
